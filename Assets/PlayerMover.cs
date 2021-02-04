c1da44543ba8f9639a003201cf4be457ed174b14
elliebababa <ellie24.huang@gmail.com>
2021-02-03 22:44:09 -0800

    Demo

---
+++ Assets/PlayerMover.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMover : NetworkBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(horizontal, 0f, vertical);
+            transform.position += movement * Time.deltaTime * moveSpeed;
+        }
+    }
+}
